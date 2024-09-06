61f3793cbd9ef3e5199694379d87c90ebfdac10f
Lukas Eberl <98182883+Lukas570@users.noreply.github.com>
2024-09-06 18:10:32 +0200

    Add files via upload

---
+++ PS3-Gameloader/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿partial class MainForm
+{
+    private System.ComponentModel.IContainer components = null;
+    private System.Windows.Forms.TextBox searchTextBox;
+    private System.Windows.Forms.ListBox resultsListBox;
+    private System.Windows.Forms.Button downloadButton;
+    private System.Windows.Forms.TextBox filePathTextBox;
+    private System.Windows.Forms.Button browseButton;
+    private System.Windows.Forms.ProgressBar downloadProgressBar;
+    private System.Windows.Forms.Label downloadProgressLabel;
+    private System.Windows.Forms.Button cancelButton; // Hinzufügen des Abbrechen-Buttons
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.downloadProgressBar = new System.Windows.Forms.ProgressBar();
+            this.downloadProgressLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(18, 58);
+            this.searchTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(1162, 26);
+            this.searchTextBox.TabIndex = 0;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+            // 
+            // resultsListBox
+            // 
+            this.resultsListBox.FormattingEnabled = true;
+            this.resultsListBox.ItemHeight = 20;
+            this.resultsListBox.Location = new System.Drawing.Point(18, 98);
+            this.resultsListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resultsListBox.Name = "resultsListBox";
+            this.resultsListBox.Size = new System.Drawing.Size(1162, 524);
+            this.resultsListBox.TabIndex = 1;
+            this.resultsListBox.SelectedIndexChanged += new System.EventHandler(this.resultsListBox_SelectedIndexChanged);
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(1070, 634);
+            this.downloadButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(112, 35);
+            this.downloadButton.TabIndex = 2;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(18, 18);
+            this.filePathTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(1040, 26);
+            this.filePathTextBox.TabIndex = 3;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(1070, 15);
+            this.browseButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(112, 35);
+            this.browseButton.TabIndex = 4;
+            this.browseButton.Text = "Add .CSV";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // downloadProgressBar
+            // 
+            this.downloadProgressBar.Location = new System.Drawing.Point(18, 634);
+            this.downloadProgressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.downloadProgressBar.Name = "downloadProgressBar";
+            this.downloadProgressBar.Size = new System.Drawing.Size(1042, 35);
+            this.downloadProgressBar.TabIndex = 5;
+            // 
+            // downloadProgressLabel
+            // 
+            this.downloadProgressLabel.AutoSize = true;
+            this.downloadProgressLabel.Location = new System.Drawing.Point(18, 678);
+            this.downloadProgressLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.downloadProgressLabel.Name = "downloadProgressLabel";
+            this.downloadProgressLabel.Size = new System.Drawing.Size(0, 20);
+            this.downloadProgressLabel.TabIndex = 6;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(471, 675);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(115, 33);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Visible = false;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 720);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.downloadProgressLabel);
+            this.Controls.Add(this.downloadProgressBar);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.resultsListBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "PS3 Gameloader";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+}
---
+++ PS3-Gameloader/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Diagnostics;
+using System.Threading;
+
+public partial class MainForm : Form
+{
+    private List<Game> games = new List<Game>();
+    private CancellationTokenSource cancellationTokenSource;
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+       
+    }
+
+    private void LoadGames(string filePath)
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";",
+            HeaderValidated = null,
+            MissingFieldFound = null,
+            BadDataFound = null
+        };
+
+        try
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap<GameMap>();
+                games = csv.GetRecords<Game>().ToList();
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error while loading the games: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+ 
+    private void SearchGames(string query)
+    {
+        var results = games.Where(g => g.Title.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+        resultsListBox.DataSource = results;
+        resultsListBox.DisplayMember = "Title";
+    }
+
+    private void searchTextBox_TextChanged(object sender, EventArgs e)
+    {
+        SearchGames(searchTextBox.Text);
+    }
+
+    private void resultsListBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        // Work in Progress
+    }
+
+    private async void downloadButton_Click(object sender, EventArgs e)
+    {
+        if (resultsListBox.SelectedItem is Game selectedGame)
+        {
+            var result = MessageBox.Show($"You really wanna download '{selectedGame.Title}'?", "Confirmation", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                using (var saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "PKG files (*.pkg)|*.pkg|All files (*.*)|*.*";
+                    saveFileDialog.FileName = selectedGame.Title;
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string savePath = saveFileDialog.FileName;
+                        cancellationTokenSource = new CancellationTokenSource();
+                        cancelButton.Visible = true;
+                        await DownloadGame(selectedGame.Url, savePath, cancellationTokenSource.Token);
+                        cancelButton.Visible = false;
+                        MessageBox.Show($"Download Complete!", "Download Complete!", MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+    }
+
+    private async Task DownloadGame(string url, string savePath, CancellationToken cancellationToken)
+    {
+        using (var client = new HttpClient())
+        {
+            try
+            {
+                var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+                response.EnsureSuccessStatusCode();
+
+                var totalBytes = response.Content.Headers.ContentLength ?? 1;
+                var buffer = new byte[8192];
+                var bytesRead = 0L;
+                var totalRead = 0L;
+                var stopwatch = Stopwatch.StartNew();
+
+                using (var contentStream = await response.Content.ReadAsStreamAsync())
+                using (var fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.None, buffer.Length, true))
+                {
+                    while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken)) > 0)
+                    {
+                        await fileStream.WriteAsync(buffer, 0, (int)bytesRead, cancellationToken);
+                        totalRead += bytesRead;
+                        var downloadRate = totalRead / (stopwatch.Elapsed.TotalSeconds * 1024 * 1024); // MB/s
+                        downloadProgressBar.Value = (int)((totalRead * 100L) / totalBytes);
+                        downloadProgressLabel.Text = $"{(totalRead / (1024 * 1024)):0.00}/{(totalBytes / (1024 * 1024)):0.00} MB ({downloadRate:0.00} MB/s)";
+                        downloadProgressBar.Refresh();
+                        downloadProgressLabel.Refresh();
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Download was terminated.", "Download Terminated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An Error occured while downloading!: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    private void cancelButton_Click(object sender, EventArgs e)
+    {
+        cancellationTokenSource?.Cancel();
+    }
+
+    private void browseButton_Click(object sender, EventArgs e)
+    {
+        using (var openFileDialog = new OpenFileDialog())
+        {
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePathTextBox.Text = openFileDialog.FileName;
+                LoadGames(openFileDialog.FileName);
+            }
+        }
+    }
+}
+
+public class Game
+{
+    public string ID { get; set; }
+    public string Title { get; set; }
+    public string Type { get; set; }
+    public string Region { get; set; }
+    public string Url { get; set; }
+    public string Rap { get; set; }
+    public string Hash { get; set; }
+    public string Description { get; set; }
+}
+
+public class GameMap : ClassMap<Game>
+{
+    public GameMap()
+    {
+        Map(m => m.ID).Index(0);
+        Map(m => m.Title).Index(1);
+        Map(m => m.Type).Index(2);
+        Map(m => m.Region).Index(3);
+        Map(m => m.Url).Index(4);
+        Map(m => m.Rap).Index(5);
+        Map(m => m.Hash).Index(6);
+        Map(m => m.Description).Index(7);
+    }
+}
